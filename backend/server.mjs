cbee3f4e7b576753daccdeef76c282f88a67c95b
mouheb htiwch <93223049+Mouuheb@users.noreply.github.com>
2025-01-17 11:49:49 +0100

    create first method

--- backend/server.mjs
+++ backend/server.mjs
@@ -22,18 +22,52 @@
 //     console.log("");
 // })
 
-// 
+// test model
 
+// import express from "express";
+// import dotenv from "dotenv";
+// import { connectDB } from "./config/db.mjs";
+
+
+// dotenv.config(); //to read the .env
+// const app = express();
+
+// app.get("/", (req,res) => {
+//     res.send("server")
+// });
+
+// console.log(process.env.MONGO_URI);
+
+// app.listen(5000, () =>{
+//     connectDB();
+//     console.log("");
+// })
+
+//
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.mjs";
+import Product from "./models/product.mjs";
 
 
 dotenv.config(); //to read the .env
 const app = express();
+app.use(express.json());
 
-app.get("/", (req,res) => {
-    res.send("server")
+app.post("/", async (req,res) => {
+    const product = req.body;
+    if(!product.name || !product.price || !product.image){
+        return res.status(400).json({ success:false, message: "Please sala7"});
+    }
+    const newproduct = new Product(product)
+
+    try {
+        await newproduct.save();
+        res.status(201).json({success:true, data: newproduct})
+    } catch (error) {
+        console.log("error ",error)
+        res.status(500).json({success:false, message:"error"})
+    }
 });
 
 console.log(process.env.MONGO_URI);
