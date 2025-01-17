9d5d5ed0c17c0aa9cf0a859c115452a2257fd035
mouheb htiwch <93223049+Mouuheb@users.noreply.github.com>
2025-01-17 13:05:22 +0100

    complete the backend

--- backend/server.mjs
+++ backend/server.mjs
@@ -89,6 +89,7 @@ import productRoutes from "./routes/product.mjs";
 
 dotenv.config(); //to read the .env
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(express.json()); //to accept json in the body
 
 app.use("/api/products", productRoutes);
@@ -96,7 +97,7 @@ app.use("/api/products", productRoutes);
 
 console.log(process.env.MONGO_URI);
 
-app.listen(5000, () =>{
+app.listen(PORT, () =>{
     connectDB();
     console.log("");
 })
