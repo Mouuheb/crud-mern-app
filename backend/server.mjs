7e80b9d1a7ee8af92517860ed132954ee94c7c5d
mouheb htiwch <93223049+Mouuheb@users.noreply.github.com>
2025-01-17 10:40:15 +0100

    env

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,8 +1,40 @@
-import express from "express";
+// test server
 
+// import express from "express";
+
+// const app = express();
+
+// app.listen(5000, () =>{
+//     console.log("dldld");
+// })
+
+// ----- first methode
+
+// import express from "express";
+
+// const app = express();
+
+// app.get("/", (req,res) => {
+//     res.send("server")
+// });
+
+// app.listen(5000, () =>{
+//     console.log("");
+// })
+
+// 
+
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config(); //to read the .env
 const app = express();
 
+app.get("/", (req,res) => {
+    res.send("server")
+});
 
+console.log(process.env.MONGO_URI);
 
 app.listen(5000, () =>{
     console.log("");
